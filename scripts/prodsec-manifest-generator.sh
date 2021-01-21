5db04254eac92febe8b2058cd228b5e00ed0785c
MStokluska <mstoklus@redhat.com>
2021-01-21 11:50:41 +0000

    update to prodsec generator

--- scripts/prodsec-manifest-generator.sh
+++ scripts/prodsec-manifest-generator.sh
@@ -3,24 +3,14 @@
 # This script generates a manifest of the integreatly-operator which includes the direct and indirect dependencies 
 # used. The generated manifest corresponds to a released version and is located in : ../rhoam-manifests/
 
-MANIFEST_TYPE=$1
-
 # Compares current manifest vs manifest generated for master
 manifest_compare() {
-    RHOAM_VERSION="master"
-    FILE_NAME="prodsec-master-from-branch-manifest.txt"
-    RHOAM_MASTER_FROM_BRANCH="prodsec-manifests/rhoam-master-from-branch-manifest.txt"
-    RHOAM_CURRENT_MASTER="prodsec-manifests/rhoam-master-manifest.txt"
-    RHOAM_SORTED_MASTER="prodsec-manifests/rhoam-master-sorted-manifest.txt"
-
-    manifest_generate
-
     # Additional sorting of the master manifest is required as it seems that prow sorts files in a different manner.
     sort -u "$RHOAM_CURRENT_MASTER" > "$RHOAM_SORTED_MASTER"
 
     MANIFESTS_DIFF=$(diff --suppress-common-lines ${RHOAM_MASTER_FROM_BRANCH} ${RHOAM_SORTED_MASTER})
     if [ ! -z "$MANIFESTS_DIFF" ]; then
-        echo "Difference found between master manifests, run make rhoam/manifest and push PR again"
+        echo "Difference found between master manifests, run `TYPE_OF_MANIFEST=master make manifest/prodsec` and push PR again"
         
         # Delete sorted files
         rm -f "$RHOAM_MASTER_FROM_BRANCH"
@@ -38,14 +28,13 @@ manifest_compare() {
 
 # Generates the manifest, it can be either master or production manifest
 manifest_generate() {
-    # Set the service name and version
-    SERVICE_NAME="services-rhoam"
-
     # Pre-filetered manifest file
     PRE_SORTED_FILE="prodsec-manifests/pre-sorted-file.txt"
 
+    env
+
     # Dependencies used
-    go mod graph | cut -d " " -f 2 | tr @ - | while read x; do echo "${SERVICE_NAME}:${RHOAM_VERSION}/$x" >> "$PRE_SORTED_FILE"; done
+    go mod graph | cut -d " " -f 2 | tr @ - | while read x; do echo "${SERVICE_NAME}:${VERSION}/$x" >> "$PRE_SORTED_FILE"; done
 
     # Remove repeating dependencies
     sort -u "$PRE_SORTED_FILE" > "prodsec-manifests/$FILE_NAME"
@@ -54,26 +43,53 @@ manifest_generate() {
     rm -f "$PRE_SORTED_FILE"
 }
 
-case $MANIFEST_TYPE in
+case $TYPE_OF_MANIFEST in
 "master")
-    RHOAM_VERSION="master"
-    FILE_NAME="prodsec-master-manifest.txt"
+    SERVICE_NAME="services-rhoam"
+    VERSION="master"
+    FILE_NAME="rhoam-master-manifest.txt"
+
     manifest_generate
     exit 0
     ;;
 "compare")
+    SERVICE_NAME="services-rhoam"
+    VERSION="master"
+    FILE_NAME="master-from-branch-manifest.txt"
+
+    RHOAM_MASTER_FROM_BRANCH="prodsec-manifests/master-from-branch-manifest.txt"
+    RHOAM_CURRENT_MASTER="prodsec-manifests/rhoam-master-manifest.txt"
+    RHOAM_SORTED_MASTER="prodsec-manifests/rhoam-master-sorted-manifest.txt"
+
+    manifest_generate
     manifest_compare
     exit 0
     ;;
-"")
-    RHOAM_VERSION=$(grep managed-api-service.v deploy/olm-catalog/managed-api-service/managed-api-service.package.yaml | tail -c 6)
-    FILE_NAME="rhoam-production-release-manifest.txt"
+"production")
+    case $OLM_TYPE in
+      "integreatly-operator")
+        OLM_TYPE="rhmi"
+        VERSION=$(grep integreatly-operator.v deploy/olm-catalog/integreatly-operator/integreatly-operator.package.yaml | tail -c 6)
+        FILE_NAME="rhmi-production-release-manifest.txt"
+        SERVICE_NAME="services-rhmi"
+        ;;
+      "managed-api-service")
+        OLM_TYPE="rhoam"
+        VERSION=$(grep managed-api-service.v deploy/olm-catalog/managed-api-service/managed-api-service.package.yaml | tail -c 6)
+        FILE_NAME="rhoam-production-release-manifest.txt"
+        SERVICE_NAME="services-rhoam"
+        ;;
+      *)
+        echo "Invalid OLM_TYPE set"
+        echo "Use \"integreatly-operator\" or \"managed-api-service\""
+        exit 1
+        ;;
+    esac
     manifest_generate
-    exit 0
     ;;
 *)
     echo "Invalid type of manifest requested"
-    echo "Use \"production\" or \"master\""
+    echo "Use \"master\",\"production\" or \"compare\""
     exit 1
     ;;
 esac
